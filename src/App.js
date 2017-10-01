bb29d579daf38819a32daf1a90c53bbc8445c111
Christian <ChrisWcs@users.noreply.github.com>
2017-10-01 13:35:15 -0600

    Turn App into class

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+class App extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            id: ""
+        };
+
+
+    }
+    
+    render() {
+        <Provider>
+            <div>
+                
+            </div>
+        </Provider>
+    }
+}
+
+export default App;
